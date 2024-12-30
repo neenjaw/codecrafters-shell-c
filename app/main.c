3f28cd1b42ddce93c78f955c0236b70c8c3e92b1
Tim Austin <tim@neenjaw.com>
2024-12-29 21:23:52 -0600

    add naive invalid command handling

--- app/main.c
+++ app/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
   // Flush after every printf
   setbuf(stdout, NULL);
 
+  // Print prompt
   printf("$ ");
 
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+
+  // Remove trailing newline
+  input[strcspn(input, "\n")] = 0;
+
+  // Handle invalid command input
+  printf("%s: command not found\n", input);
+
   return 0;
 }
